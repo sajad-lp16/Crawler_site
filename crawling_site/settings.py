d5c3fddbfbc9d58a0c075b9e4de1f041bed00c87
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-08-26 21:05:41 +0430

    add media and static root and update requirements.txt

--- crawling_site/settings.py
+++ crawling_site/settings.py
@@ -90,6 +90,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static config
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+STATIC_ROOT = 'static_cdn'
+
+# Media config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = config('DEFAULT_AUTO_FIELD')
