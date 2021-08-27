7e591af98dc38f714287aadb305ac579425eb115
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-08-27 16:24:13 +0430

    add some required constant values "url and model mapper"

--- movies/management/commands/constants.py
+++ movies/management/commands/constants.py
@@ -1,1 +1,10 @@
 URL = 'https://movieland.cc/'
+TRANSLATOR = {
+    'نوع فیلم': 'movie_type',
+    'سال تولید': 'production_year',
+    'کارگردان': 'director',
+    'بازیگران': 'actors',
+    'خلاصه داستان': 'description',
+    'زبان': 'movie_language',
+    'امتیاز': 'rating',
+}
