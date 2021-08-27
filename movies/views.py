7b7e3671b82602d1148e1ecd4e9b5fe98c810ea0
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-08-27 18:14:23 +0430

    add main page view

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class MainPage(generic.View):
+    def get(self, request):
+        return render(request, 'home.html')
