cc155510ecaf3ef35de1c24ba26fc183bffa7197
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-08-27 16:57:23 +0430

    customize admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from . import models
 
 
+class MovieInline(admin.TabularInline):
+    model = models.Movie
+
+
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = 'title', 'create_time', 'update_time'
+    inlines = [MovieInline]
 
 
 @admin.register(models.Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = 'name', 'rating', 'production_year'
+    list_display = 'name', 'rating', 'production_year', 'director'
+    search_fields = 'name', 'director', 'genre'
+    list_filter = 'genre',
+    list_editable = 'director', 'rating', 'production_year'
