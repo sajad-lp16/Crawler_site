3627db39f3902c6e3e2de70774f92bc4a6d12abc
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-02 22:34:47 +0430

    implement views to handle detail and list views

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views import generic
 
+from . import models
 
-class MainPage(generic.View):
-    def get(self, request):
-        return render(request, 'home.html')
+
+class Home(generic.ListView):
+    model = models.Movie
+    template_name = 'home.html'
+    context_object_name = 'movies'
+    paginate_by = 18
+
+
+class MoviesList(generic.ListView):
+    paginate_by = 18
+    template_name = 'home.html'
+    context_object_name = 'movies'
+
+    def get_queryset(self):
+        return models.Movie.objects.filter(genre__slug=self.kwargs['slug'])
+
+
+class MovieDetail(generic.DetailView):
+    model = models.Movie
+    template_name = 'movie_detail.html'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(models.Movie, slug=self.kwargs['slug'], genre__slug=self.kwargs['slug1'])
