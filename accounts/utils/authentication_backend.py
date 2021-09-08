fec2fa920788531ebf1d31e7a2eb5b30d1cfd0dc
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-09 00:41:15 +0430

    add new authentication class and set it in settings

---
+++ accounts/utils/authentication_backend.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+User = get_user_model()
+
+
+class AuthenticationBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return
+        user = User.objects.filter(
+            Q(username=username) | Q(email=username)
+        ).first()
+        if user is None:
+            return
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
