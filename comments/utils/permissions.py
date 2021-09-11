ec13ee264b9069d47836c19d779a4d238063b1ff
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-11 23:36:20 +0430

    add custom permissions

--- comments/utils/permissions.py
+++ comments/utils/permissions.py
@@ -6,4 +6,4 @@ class CommentOwnerPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj == request.user
+        return obj.user == request.user
